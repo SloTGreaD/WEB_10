f61a5eaf869b35d044cc748951475a61b1ff52bd
SloTGreaD <148031433+SloTGreaD@users.noreply.github.com>
2024-04-16 12:53:59 +0800

    Delete hm_project directory

--- hm_project/quotes/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path
-
-app_name = 'quotes'
-
-urlpatterns = [
-    path('', views.main, name='root'),
-    path('<int:page>', views.main, name='root_paginate')
-]
